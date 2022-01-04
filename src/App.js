a902b968158de4d254fd3eccb0d66c1884954f27
vagrant <vagrant@vagrant.vm>
2022-01-04 14:43:15 +0000

    station4

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = React.useState('https://images.dog.ceo/breeds/hound-afghan/n02088094_4521.jpg');
   return (
     <div>
       <header>
         <h1>Dog App</h1>
       </header>
       <p>犬の画像を表示するサイトです</p>
-      <img src="https://images.dog.ceo/breeds/hound-afghan/n02088094_4521.jpg"></img>
+      <img src={dogUrl}></img>
     </div>
-  )
+  );
 }
